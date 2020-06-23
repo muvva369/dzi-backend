b247939364de19702b62275f9769703bf320b1bc
Himanshu <hctohana@gmail.com>
2020-06-23 22:54:15 +0530

    Deleted unwanted code
    Deleted unwanted code

--- routing.js
+++ routing.js
@@ -7,33 +7,6 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 
-router.get('/setupDb', (req, res, next) => {
-    create.setupDb().then((data) => {
-        res.send(data)
-    }).catch((err) => {
-        next(err)
-    })
-})
-
-
-// to post details to mtplCalculator collection
-router.post('/mtpCalculator', (req, res, next) => {
-    let details = req.body;
-    service.addmtpDetails(details).then((response) => {
-        res.json(response);
-
-    }).catch((err) => next(err))
-})
-
-
-// get details from mtplcalculator collection  by registration no.
-router.get('/getdetails/:regno', (req, res, next) => {
-    let regNo = req.params.regno;
-    service.getDetails(regNo).then((response) => {
-        console.log(response);
-        res.json(response);
-    }).catch((err) => next(err));
-})
 
 // to post details to purchasePolicy collection
 router.post('/purchasePolicy', (req, res, next) => {
--- service.js
+++ service.js
@@ -4,33 +4,7 @@ let service={}
 
 
 
-//adding details to mtplCalculator collection
-service.addmtpDetails = ((details) => {
-    return db.insertmtpDetails(details).then((data) => {
-        if (data == null) {
-            let err = new Error("failed to insert details!!");
-            err.status = 404;
-            throw err;
-        } else {
-                return {"message":"details added successfully!!"};
-        }
-    })
-})
 
-
-//getting details by regNo
-service.getDetails = ((regNo) => {
- 
-    return db.getData(regNo).then((data) => {
-        if (data == null) {
-            let err = new Error("details not available!!");
-            err.status = 404;
-            throw err;
-        } else {
-                return data;
-        }
-    })
-})
 
 //adding details to purchasePolicy collection
 service.addPolicyDetails = ((details) => {
