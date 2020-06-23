0a4f366e4ba706d969c7f2e48ca9f2547a88fefe
SidRepository1 <siddharthbrahambhatt6666@gmail.com>
2020-06-23 21:31:24 +0530

    Added swagger API

--- app/models/userModel/userModel.js
+++ app/models/userModel/userModel.js
@@ -1,34 +1,34 @@
 const { verifyToken } = require("../../utils/authUtils");
 
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define("user", {
-      id: {
-        type : Sequelize.INTEGER,
-        primaryKey: true
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        len: [1,50]
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        len: [1,1000]
-      },
-      mobile: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        field: 'mobile',
-        isNumeric: true,
-        validate: {
-            isLongEnough: function (val) {
-                if (val.length != 10) {
-                    throw new Error("Please enter 10 digit Valid Phone Number")
-                }
-            }
+  const User = sequelize.define("user", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      len: [1, 50]
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      len: [1, 1000]
+    },
+    mobile: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      field: 'mobile',
+      isNumeric: true,
+      validate: {
+        isLongEnough: function (val) {
+          if (val.length != 10) {
+            throw new Error("Please enter 10 digit Valid Phone Number")
+          }
         }
+      }
 
     },
     totpToken: {
@@ -37,10 +37,10 @@ module.exports = (sequelize, Sequelize) => {
     },
     createdAt: true,
     updatedAt: true
-     },{
-       tableName:'users',
-       timestamps: true
-    });
-  
-    return User;
-  };
+  }, {
+    tableName: 'users',
+    timestamps: true
+  });
+
+  return User;
+};
--- app/server.js
+++ app/server.js
@@ -3,9 +3,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
 
 const user = require('./routes/userRoutes/userRoutes');
- 
+
 
 var corsOptions = {
   origin: "http://localhost:3001"
@@ -20,7 +23,102 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use('/user',user);
+app.use('/user', user);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    "swagger": "2.0",
+    "info": {
+      "version": "1.0.0",
+      "title": "Login API",
+      "description": "Login API",
+      "license": {
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT"
+      }
+    },
+    "paths": {
+      "/user": {
+        "get": {
+          "tags": [
+            "Get User API"
+          ],
+          "summary": "To get individual user  details by email of user",
+          "responses": {
+            "200": {
+              "description": "OK"
+            },
+            "404": {
+              "description": "Failed. Bad Request"
+            }
+          }
+        }
+      },
+      "/user/createUserfirst": {
+        "post": {
+          "tags": [
+            "Create User API"
+          ],
+          "summary": "To Create User using email and password and assign token to it.",
+          "requestBody": {
+            "description": "User Object",
+            "required": true,
+            "content": "application/json"
+          },
+          "parameters": [
+            {
+              "in": "body",
+              "name": "body",
+              "required": true,
+              "description": "User with new values",
+            }
+          ],
+          "responses": {
+            "200": {
+              "description": "OK"
+            },
+            "404": {
+              "description": "Failed. Bad Request"
+            }
+          }
+        }
+      },
+      "/user/loginUser": {
+        "post": {
+          "tags": [
+            "Login User API"
+          ],
+          "summary": "To Login User using email and password",
+          "requestBody": {
+            "description": "User Object",
+            "required": true,
+            "content": "application/json"
+          },
+          "parameters": [
+            {
+              "in": "body",
+              "name": "body",
+              "required": true,
+              "description": "Login User Data",
+            }
+          ],
+          "responses": {
+            "200": {
+              "description": "OK"
+            },
+            "404": {
+              "description": "Failed. Bad Request"
+            }
+          }
+        }
+      }
+    }
+  },
+  apis: ["./routes/userRoutes/userRoutes/*.js"]
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 
 // simple route
